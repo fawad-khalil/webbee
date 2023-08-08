8a3e8920bd59f930a5237d95d34fc4534253c070
Fawad Khalil <fawad-khalil@hotmail.com>
2023-08-08 05:40:30 +0500

    add cinema system migration Note: I wanted to create separate migrations but apprarently from the nature of file and migration name, it seems there needs to be only one migration. I am not sure.

--- express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,4 +1,4 @@
-import { QueryInterface } from 'sequelize';
+import { QueryInterface, Sequelize, DataTypes } from 'sequelize';
 
 export default {
   /**
@@ -12,27 +12,362 @@ export default {
    ## User Stories
 
    **Movie exploration**
-   * As a user I want to see which films can be watched and at what times
-   * As a user I want to only see the shows which are not booked out
+   * As a user I want to see which films can be watched and at what times - DONE
+   * As a user I want to only see the shows which are not booked out - DONE
 
    **Show administration**
-   * As a cinema owner I want to run different films at different times
-   * As a cinema owner I want to run multiple films at the same time in different showrooms
+   * As a cinema owner I want to run different films at different times - DONE
+   * As a cinema owner I want to run multiple films at the same time in different showrooms - DONE
 
    **Pricing**
-   * As a cinema owner I want to get paid differently per show
-   * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
+   * As a cinema owner I want to get paid differently per show - DONE
+   * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat - DoNE
 
    **Seating**
-   * As a user I want to book a seat
-   * As a user I want to book a vip seat/couple seat/super vip/whatever
-   * As a user I want to see which seats are still available
-   * As a user I want to know where I'm sitting on my ticket
-   * As a cinema owner I don't want to configure the seating for every show
+   * As a user I want to book a seat - DONE
+   * As a user I want to book a vip seat/couple seat/super vip/whatever - DONE
+   * As a user I want to see which seats are still available - DONE
+   * As a user I want to know where I'm sitting on my ticket - DONE
+   * As a cinema owner I don't want to configure the seating for every show - DONE
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   up: (queryInterface: QueryInterface): Promise<void> => {
-    throw new Error('TODO: implement migration in task 4');
+    // throw new Error('TODO: implement migration in task 4');
+    queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('DBOperations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    });
+
+    queryInterface.createTable('RolesAccessLists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      roleId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Roles',
+          key: 'id',
+        },
+      },
+      tableName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      op: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'DBOperations',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('UserRoles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      roleId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Roles',
+          key: 'id',
+        },
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('Films', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('ShowRooms', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('Shows', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      filmId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Films',
+          key: 'id',
+        },
+      },
+      showTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      showRoomId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'ShowRooms',
+          key: 'id',
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: 'Films',
+          key: 'id',
+        },
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('Bookings', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      showId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Shows',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    queryInterface.createTable('SeatTypes', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      premiumPercentage: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
+
+    return queryInterface.createTable('RoomSeatTypes', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      roomId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'ShowRooms',
+          key: 'id',
+        },
+      },
+      seatTypeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'SeatTypes',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+    });
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
